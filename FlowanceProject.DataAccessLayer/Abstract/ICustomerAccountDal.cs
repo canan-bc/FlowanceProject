a9ee836027c2316157d40e523afca1c2d18dd1d4
Canan <useryedek56@gmail.com>
2025-10-19 02:07:15 +0300

    bağlantılarım

---
+++ FlowanceProject.DataAccessLayer/Abstract/ICustomerAccountDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowanceProject.EntityLayer.Concrete;
+
+namespace FlowanceProject.DataAccessLayer.Abstract
+{
+    public interface ICustomerAccountDal:IGenericDal<CustomerAccount>
+    {
+
+
+    }
+}
